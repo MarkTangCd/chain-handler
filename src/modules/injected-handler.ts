9b3dd42dbfa775ebca7a6ab602785e06ce6212da
programmer <programmer@test.com>
2022-02-10 09:38:28 +0800

    done

--- src/modules/injected-handler.ts
+++ src/modules/injected-handler.ts
@@ -33,38 +33,40 @@ class InjectedHandler extends Base {
       });
   }
 
-  async switchNetwork(network: string, callback = () => {}) {
-    // Checking to connect status of the wallet.
-    let address = await this.getAddress();
-    if (!address) {
-      throw new Error('Please connect your wallet first');
-    }
-
-    let values = Object.values(Networks);
-    if (!network) {
-      throw new Error('The param cannot be empty.');
-    } else if (values.indexOf(network) === -1){
-      throw new Error('The param is wrong.');
-    }
-
-    let detail: any = NetworksDetails.find((item) => item.id === network);
-    try {
-      await window.ethereum.request({
-        method: 'wallet_switchEthereumChain',
-        params: [{chainId: detail.config.chainId}]
-      })
-      callback();
-    } catch (err: any) {
-      if (err.code === NOT_THE_CHAIN) {
-        await window.ethereum.request({
-          method: 'wallet_addEthereumChain',
-          params: [detail.config, address]
-        });
-        callback();
-      } else {
-        console.error(err);
+  switchNetwork(network: string) {
+    return new Promise(async (resolve, reject) => {
+      // Checking to connect status of the wallet.
+      let address = await this.getAddress();
+      if (!address) {
+        throw new Error('Please connect your wallet first');
       }
-    }
+
+      let values = Object.values(Networks);
+      if (!network) {
+        throw new Error('The param cannot be empty.');
+      } else if (values.indexOf(network) === -1){
+        throw new Error('The param is wrong.');
+      }
+
+      let detail: any = NetworksDetails.find((item) => item.id === network);
+      try {
+        await window.ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{chainId: detail.config.chainId}]
+        })
+        resolve(true);
+      } catch (err: any) {
+        if (err.code === NOT_THE_CHAIN) {
+          await window.ethereum.request({
+            method: 'wallet_addEthereumChain',
+            params: [detail.config, address]
+          });
+          resolve(true);
+        } else {
+          reject(err);
+        }
+      }
+    });
   }
 }
 
